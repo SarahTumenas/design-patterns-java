01ac1661352b9aa814dfdc870a775103936b9867
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-20 16:27:27 -0300

    Aplicando o pattern Command

--- src/main/java/br/com/alura/loja/TestesPedidos.java
+++ src/main/java/br/com/alura/loja/TestesPedidos.java
@@ -1,23 +1,20 @@
 package br.com.alura.loja;
 
-import br.com.alura.loja.orcamento.Orcamento;
-import br.com.alura.loja.pedido.Pedido;
+
+import br.com.alura.loja.pedido.GeraPedido;
 
 import java.math.BigDecimal;
-import java.time.LocalDateTime;
 
 public class TestesPedidos {
 
     public static void main(String[] args) {
+        //simulando a interface de linha de comando
+        String cliente =args[0];
+        BigDecimal valorOrcamento = new BigDecimal(args[1]);
+        int quantidadeItens = Integer.parseInt(args[2]);
 
-        Orcamento orcamento = new Orcamento(new BigDecimal("500"), 4);
-        String cliente = "Ana da Silva";
-        LocalDateTime data = LocalDateTime.now();
-        Pedido pedido = new Pedido(cliente, data, orcamento);
-
-        System.out.println("Salvar pedido");
-        System.out.println("Enviar email");
-
+        GeraPedido gerador = new GeraPedido(cliente, valorOrcamento, quantidadeItens);
+        gerador.executa();
 
     }
 }
