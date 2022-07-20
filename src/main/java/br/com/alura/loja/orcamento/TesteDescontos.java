5725385b6f296f8b7671f1d4fcdf7219a579ddd3
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-20 11:32:50 -0300

    Chain of Resposability - calculando descontos

---
+++ src/main/java/br/com/alura/loja/orcamento/TesteDescontos.java
@@ -0,0 +1,26 @@
+package br.com.alura.loja.orcamento;
+
+import br.com.alura.loja.desconto.CalculadoraDeDescontos;
+
+
+
+import java.math.BigDecimal;
+
+public class TesteDescontos {
+
+    public static void main(String[] args) {
+        Orcamento orcamento = new Orcamento(new BigDecimal("200"), 6);
+
+        Orcamento orcamento2 = new Orcamento(new BigDecimal("200"), 4);
+
+        Orcamento orcamento3 = new Orcamento(new BigDecimal("600"), 4);
+
+        CalculadoraDeDescontos calculadora = new CalculadoraDeDescontos();
+
+        System.out.println(calculadora.calcular(orcamento));
+
+        System.out.println(calculadora.calcular(orcamento2));
+
+        System.out.println(calculadora.calcular(orcamento3));
+    }
+}
