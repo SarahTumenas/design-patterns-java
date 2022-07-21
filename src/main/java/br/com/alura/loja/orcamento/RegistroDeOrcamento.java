62f64ed6edd1b81403f95048baba2c23103a73b3
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-21 13:10:58 -0300

    Api de registro de orçamento

---
+++ src/main/java/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -0,0 +1,14 @@
+package br.com.alura.loja.orcamento;
+
+public class RegistroDeOrcamento {
+
+    public void registrar(Orcamento orcamento){
+
+        //chamda Http para a API externa
+        //URL Connection - java puro
+        //Http Client biblioteca do apache
+        //lib Rest
+
+
+    }
+}
