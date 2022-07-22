bced0331394f6861e761e9b9ac12d18e2d1def25
Sarah Tumenas <sarah.marques@spread.com.br>
2022-07-22 11:29:51 -0300

    Aplicando o pattern Adapter

--- src/main/java/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
+++ src/main/java/br/com/alura/loja/orcamento/RegistroDeOrcamento.java
@@ -1,6 +1,17 @@
 package br.com.alura.loja.orcamento;
 
+import br.com.alura.loja.DomainException;
+import br.com.alura.loja.http.HttpAdapter;
+
+import java.util.Map;
+
 public class RegistroDeOrcamento {
+
+    private HttpAdapter http;
+
+    public RegistroDeOrcamento(HttpAdapter http) {
+        this.http = http;
+    }
 
     public void registrar(Orcamento orcamento){
 
@@ -9,6 +20,17 @@ public class RegistroDeOrcamento {
         //Http Client biblioteca do apache
         //lib Rest
 
+        if (!orcamento.isFinalizado()) {
+            throw new DomainException("Orçamento não finalizado!");
+        }
+
+        String url = "http://api.externa/orcamentos";
+        Map<String, Object> dados = Map.of(
+               "valor", orcamento.getValor(),
+               "quantidadeItens", orcamento.getQuantidadeItens()
+        );
+
+        http.post(url,dados);
 
     }
 }
